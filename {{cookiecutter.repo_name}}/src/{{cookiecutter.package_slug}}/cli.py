3930803fb9bfd5ea935babd686f7577a103307b5
adeslatt <adeslat@scitechcon.org>
2025-04-07 15:41:02 -0400

    needed to inspect all variable names for the cookiecutter

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.package_slug}}/cli.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.package_slug}}/cli.py
@@ -1,4 +1,4 @@
-# src/{{ package_slug }}/cli.py
+# src/{{cookiecutter.package_slug}}/cli.py
 import typer
 app = typer.Typer()
 
@@ -10,16 +10,16 @@ def hello(name: str):
 if __name__ == "__main__":
     app()
 
-# src/{{ package_slug }}/core.py
+# src/{{cookiecutter.package_slug}}/core.py
 def do_work():
     return "work done"
 
-# src/{{ package_slug }}/utils.py
+# src/{{cookiecutter.package_slug}}/utils.py
 def add(x, y):
     return x + y
 
 # tests/test_cli.py
 def test_add():
-    from {{ package_slug }}.utils import add
+    from {{cookiecutter.package_slug}}.utils import add
     assert add(2, 2) == 4
 
